084b163bab90379fa8218e2f838ce1f5d4025c8e
Jeremy Cook <Jcook61592@gmail.com>
2024-09-04 01:29:11 -0400

    Tooltip from MUI

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ export default function App() {
         <Hero />
         <About />
         <Technologies />
-        <Projects />
         <Experience />
+        <Projects />
       </div>
     </div>
   );
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,6 +6,7 @@ import { BiLogoTypescript } from "react-icons/bi";
 import { TbBrandReactNative } from "react-icons/tb";
 import { RiNodejsLine } from "react-icons/ri";
 import { motion } from "framer-motion";
+import { Tooltip } from "@mui/material";
 
 const iconVariants = (duration) => ({
   initial: { y: -10 },
@@ -35,62 +36,69 @@ const Technologies = () => {
         initial={{ x: -100, opacity: 0 }}
         transition={{ duration: 1.5 }}
         className="flex flex-wrap items-center justify-center gap-4">
-        <motion.div
-          variants={iconVariants(2.5)}
-          initial="initial"
-          animate="animate"
-          title="React"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiReactjsLine className="text-7xl text-cyan-400" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(3)}
-          initial="initial"
-          animate="animate"
-          title="React Native"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <TbBrandReactNative className="text-7xl text-cyan-400" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(5)}
-          initial="initial"
-          animate="animate"
-          title="Nextjs"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <TbBrandNextjs className="text-7xl" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(2)}
-          initial="initial"
-          animate="animate"
-          title="Nodejs"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiNodejsLine className="text-7xl text-green-500" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(6)}
-          initial="initial"
-          animate="animate"
-          title="TypeScript"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <BiLogoTypescript className="text-7xl text-blue-600" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(3)}
-          initial="initial"
-          animate="animate"
-          title="PostgreSQL"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </motion.div>
-        <motion.div
-          variants={iconVariants(5.5)}
-          initial="initial"
-          animate="animate"
-          title="Tailwind"
-          className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiTailwindCssLine className="text-7xl text-cyan-200" />
-        </motion.div>
+        <Tooltip title="React">
+          <motion.div
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <RiReactjsLine className="text-7xl text-cyan-400" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="React Native">
+          <motion.div
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <TbBrandReactNative className="text-7xl text-cyan-400" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="Nextjs">
+          <motion.div
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <TbBrandNextjs className="text-7xl" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="Nodejs">
+          <motion.div
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <RiNodejsLine className="text-7xl text-green-500" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="TypeScript">
+          <motion.div
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <BiLogoTypescript className="text-7xl text-blue-600" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="PostgreSQL">
+          <motion.div
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <BiLogoPostgresql className="text-7xl text-sky-700" />
+          </motion.div>
+        </Tooltip>
+        <Tooltip title="Tailwind">
+          <motion.div
+            variants={iconVariants(5.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <RiTailwindCssLine className="text-7xl text-cyan-200" />
+          </motion.div>
+        </Tooltip>
       </motion.div>
     </div>
   );
