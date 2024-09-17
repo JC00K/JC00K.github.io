8f8416fbdd61e3377fdaef993b724ecf2fa4c1c4
Jeremy Cook <Jcook61592@gmail.com>
2024-09-16 20:35:22 -0400

    More grammar updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,53 +1,59 @@
-export const HERO_CONTENT = `I’m a full stack developer based in NY. I enjoy creating scalable web applications and am always eager to learn new things. With 3 years of experience and a boot camp background, I’m skilled in front-end technologies like React and Next.js, and databases like PostgreSQL, and MySQL. My goal is To keep growing in tech and deliver outstanding user experiences.`;
+export const HERO_CONTENT =
+  "I'm a full stack developer based in New York with a passion for creating scalable web applications. With three years of experience and a background from a coding bootcamp, I've honed my skills in front-end technologies like React and Next.js, as well as databases such as PostgreSQL and MySQL. I'm always eager to learn new things and stay current with the latest industry trends. My goal is to continue growing in the tech field while delivering outstanding user experiences through innovative and efficient solutions.";
 
-export const ABOUT_TEXT = `I’m a boot camp graduate with a background in the sports media streaming industry, having worked with Major League Baseball and Disney Streaming Services. At Full Stack Academy, I honed my skills in building web applications using React, Node.js, Express, and various databases. My passion for technology stems from a curiosity about how things work at a granular level and a desire for continuous learning. I bring a team-player mindset and a coach’s mentality from my previous career. Outside of coding, you can find me playing sports (and esports), gaming with friends, or enjoying some anime.`;
+export const ABOUT_TEXT =
+  "I'm a bootcamp graduate with a unique background in sports media streaming, having worked with Major League Baseball and Disney Streaming Services. My education at Full Stack Academy honed my skills in building web applications using React, Node.js, Express, and various databases. My passion for technology is driven by a curiosity to understand how things work at a granular level and a commitment to continuous learning. Drawing from my previous career, I bring a team-player mindset and a coaches mentality to my work as a developer. This combination of technical skills and collaborative approach allows me to contribute effectively to any project. When I'm not coding, you can find me playing sports (including esports), gaming with friends, or enjoying anime. At Full Stack Academy, I honed my skills in building web applications using React, Node.js, Express, and various databases. My passion for technology stems from a curiosity about how things work at a granular level and a desire for continuous learning. I bring a team-player mindset and a coaches mentality from my previous career. Outside of coding, you can find me playing sports (and esports), gaming with friends, or enjoying some anime.";
 
 export const EXPERIENCES = [
   {
-    year: "March 2024 - Present",
+    year: "Mar 2024 - Present",
     role: "Front-End Engineer (Contract)",
     company: "YugiTory",
     description:
-      "Developed a mobile app for managing Yu-Gi-Oh! card collections and updating decks for competitive play. Built with React Native, Expo, Python, EC2, RDS, and JavaScript.",
+      "Developed a mobile app for Yu-Gi-Oh! card collection management and competitive deck updates using React Native, Expo, Python, EC2, RDS, and JavaScript.",
     technologies: ["React Native", "JavaScript", "Expo", "AWS", "Python"],
   },
   {
     year: "Oct 2021 - Apr 2022",
     role: "Software Engineer Intern",
     company: "ThriveFantasy",
-    description: `Enhanced web application user experience by researching and addressing user pain points. Boosted user contest sign-ups by 20% by developing and integrating filters based on entry fees and available prop offerings.`,
+    description:
+      "Improved web application user experience through targeted research and pain point resolution. Implemented entry fee and prop offering filters, resulting in a 20% increase in user contest sign-ups.",
     technologies: ["JavaScript", "React", "Node.js", "AWS", "Java"],
   },
   {
     year: "Apr 2020 - Dec 2020",
     role: "Resolutions Specialist",
     company: "Luxottica Group",
-    description: `Assisted with resolving backend client issues, including credit/debit concerns, tax certification, partnership access (brand-specific), and product delivery discrepancies for up to 100 accounts daily.`,
+    description:
+      "Resolved backend client issues for up to 100 accounts daily, addressing credit/debit concerns, tax certification, brand-specific partnership access, and product delivery discrepancies.",
     technologies: ["CRM", "SAP", "BackOffice", "Moxie"],
   },
   {
     year: "Nov 2018 - Dec 2020",
     role: "Customer Support Coordinator",
     company: "Luxottica Group",
-    description: `Resolved issues for up to 100 accounts daily, including placing product orders and managing credit and debit requests on a case-by-case basis.`,
+    description:
+      "Managed daily operations for up to 100 accounts, including processing product orders and handling credit and debit requests on a case-by-case basis.",
     technologies: ["CRM", "SAP"],
   },
   {
     year: "Jan 2016 - Oct 2018",
     role: "Quality of Experience (QoE) Engineer",
     company: "Disney Streaming Services",
-    description: `Supported development teams by conducting device and application testing for 1.8 million concurrent subscribers across platforms including iOS, Android, Web, Roku, Apple TV, Amazon Fire TV, Xbox One, PlayStation 3 & 4, Samsung Smart TV, and NVIDIA.`,
+    description:
+      "Supported development teams by conducting comprehensive device and application testing for 1.8 million concurrent subscribers across multiple platforms, including iOS, Android, Web, Roku, Apple TV, Amazon Fire TV, Xbox One, PlayStation 3 & 4, Samsung Smart TV, and NVIDIA.",
     technologies: ["Jira", "Python", "AWS", "Charles Proxy Capture", "Fiddler"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "Video Player",
+    title: "Video Sharing Platform",
     repo: "https://github.com/JC00K/educationalvideoplayer",
     year: "2024",
     description:
-      "Developed a site where users can upload videos for others to view, and comment.",
+      "Developed a dynamic website enabling users to upload, view, and comment on videos, fostering user engagement and content creation.",
     technologies: ["React", "JavaScript", "Vite"],
   },
   {
@@ -55,7 +61,7 @@ export const PROJECTS = [
     repo: "https://github.com/Stat-to-Stat/StatToStatv2",
     year: "2024",
     description:
-      "Developed a site for comparing player and team stats in the NHL. Originally built in 2021 with React, Material UI, and Sass, it is now being updated with React, TypeScript, Material UI, and the NHL’s new API. The update will introduce a new filter feature for customized stat comparisons.",
+      "Created a comprehensive site for comparing NHL player and team statistics. Initially built in 2021 using React, Material UI, and Sass. Currently updating the platform with React, TypeScript, Material UI, and the NHL's new API. The upcoming version will feature an advanced filter system for customized stat comparisons, enhancing user experience and data analysis capabilities.",
     technologies: ["TypeScript", "React", "Axios", "Material UI"],
   },
   {
@@ -63,7 +69,7 @@ export const PROJECTS = [
     repo: "https://github.com/RoulEAT/RoulEAT",
     year: "2023",
     description:
-      "A site that lets users spin a food wheel to receive restaurant recommendations based on the selected cuisine.",
+      'Designed an interactive web application featuring a "food wheel" that provides restaurant recommendations based on user-selected cuisines. This innovative approach adds an element of fun to the dining decision process while showcasing local eateries.',
     technologies: ["TypeScript", "React", "Axios"],
   },
 ];
